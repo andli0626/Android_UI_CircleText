2f60477cbef7e06d3363e554f0f049ea943e6012
李林 <ww1095@163.com>
2014-01-17 11:40:56 +0800

    自定义跑马灯效果

--- src/com/epoint/epointuniversal/MainActivity.java
+++ src/com/epoint/epointuniversal/MainActivity.java
@@ -5,15 +5,22 @@ import android.os.Bundle;
 
 import com.epoint.epointuniversal.pjq.R;
 
+/**
+ * 
+ * @author lilin
+ * @date 2014-1-17 上午11:37:25
+ * @annotation 自定义跑马灯效果
+ */
 public class MainActivity extends Activity {
 
-	MarqueeText marqueeTextView;
+	ScrollTextView scrollText;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		scrollText = (ScrollTextView) findViewById(R.id.scrolltxt);
+		scrollText.setText("青海省人民政府行政服务和公共资源交易中心    青海省人民政府行政服务和公共资源交易中心");
 	}
-
 }
