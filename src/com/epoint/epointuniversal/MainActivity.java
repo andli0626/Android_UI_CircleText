49f8c7f0f297ec0f8dd4c44334df25f040c4e44e
李林 <ww1095@163.com>
2014-01-17 11:12:40 +0800

    1

--- src/com/epoint/epointuniversal/MainActivity.java
+++ src/com/epoint/epointuniversal/MainActivity.java
@@ -2,17 +2,6 @@ package com.epoint.epointuniversal;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.View;
-import android.view.WindowManager;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.epoint.epointuniversal.pjq.R;
 
